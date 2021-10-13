dbc227358a525a773ec7ae0f2edd164237eeb54d
Tim Jackson <timjacksonm@gmail.com>
2021-10-12 21:14:13 -0500

    Installing dependencies and creating file structure -added @sanity/client -added framer-motion -added react-icons -added styled-components -created components folder -created files for different sections of my portfolio inside of components folder -removed app.css -added cms to git ignore - sanity studio browser page -updated app.js with new components / styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
-function App() {
-  return <div className="App">timjacksonm portfolio website</div>;
-}
+const GlobalStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #000;
+  color: #fff;
+`;
+const App = () => {
+  return (
+    <GlobalStyles>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </GlobalStyles>
+  );
+};
 
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Contact = () => {
+  return <div>Contact</div>;
+};
+
+export default Contact;
