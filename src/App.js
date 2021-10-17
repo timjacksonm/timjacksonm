3f33dc458ea066c829c31b17b7ce3e9fd0de576e
Tim Jackson <76879054+timjacksonm@users.noreply.github.com>
2021-10-16 22:56:27 -0500

    Temporary/home page v2 (#3)
    * various updates to home screen
    -added MontserratBold font
    -created globalHelpers file that contains mediaquery variables
    -stlying updates to make home page mobile responsive
    -updated link container styles and added a line
    -added navbar under title
    
    * home page updates
    -moved ReactToolTip to app.js so it works globally
    -moved icons to their own component
    -mobile responive updates
    -updated navbar with a hamburger menu
    
    * last update to v2
    -added full site coming soon message back to home
    -removed console.log

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import ReactTooltip from 'react-tooltip';
 import CalibreBoldItalic from './assets/Calibre-BoldItalic.ttf';
+import MontserrratBold from './assets/Montserrat-ExtraBold.ttf';
 
 const GlobalStyles = styled.div`
   display: flex;
@@ -11,12 +13,17 @@ const GlobalStyles = styled.div`
     font-family: 'CalibreBoldItalic';
     src: url(${CalibreBoldItalic}) format('truetype');
   }
+  font-face {
+    font-family: 'MontserrratBold';
+    src: url(${MontserrratBold}) format('truetype');
+  }
 `;
 const App = () => {
   return (
     <GlobalStyles>
       <Navbar />
       <Home />
+      <ReactTooltip />
     </GlobalStyles>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { device } from '../globalHelpers';
 import { ReactComponent as GithubIcon } from '../assets/github-icon.svg';
 import { ReactComponent as TwitterIcon } from '../assets/twitter.svg';
-import { FaLinkedin } from 'react-icons/fa';
+import { FaLinkedin, FaFilePdf, FaBars, FaTimes } from 'react-icons/fa';
 
 const SectionContent = styled.div`
   position: fixed;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  z-index: 1;
+  @media ${device.laptop} {
+    top: 38vh;
+  }
+`;
+const HamburgerMenu = styled(FaBars)`
+  margin: 1em;
+  width: 1.5em;
+  height: 1.5em;
+  @media ${device.laptop} {
+    display: none;
+  }
+`;
+const LinkContainer = styled.div`
+  position: relative;
+  display: ${({ open }) => (open ? 'flex' : 'none')};
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  flex: 1;
+  width: 3em;
+  min-height: 11em;
+  margin: 1em 0;
+  background-color: #c8eff9;
+  padding: 1em 0;
+  border-radius: 5px;
+  @media ${device.laptop} {
+    display: flex;
+    margin: 3em;
+  }
+`;
+const Line = styled.span`
+  @media ${device.laptop} {
+    position: absolute;
+    top: 50%;
+    right: -40px;
+    width: 35px;
+    border-bottom: 2px solid #f27d42;
+  }
+`;
+const Link = styled.a`
+  text-decoration: none;
+  cursor: pointer;
+  height: 2em;
+  color: #fff;
   display: flex;
   align-items: center;
-  width: 100%;
-  max-height: 10%;
-  z-index: 50;
-`;
-export const LinkContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  flex: 1;
-  margin: 3em;
-`;
-export const Link = styled.a`
-  text-decoration: none;
-  margin: 0 0.5em;
-  cursor: pointer;
-  color: #fff;
   & svg {
     width: fit-content;
-    height: 2.8vh;
+    height: 1.25em;
   }
   &:hover {
-    color: #f27d42;
+    & svg {
+      transition: height 0.5s;
+      height: 1.5em;
+    }
     & svg path {
       fill: #f27d42;
     }
   }
+  @media ${device.laptop} {
+    & svg {
+      height: 1.75em;
+    }
+    &:hover {
+      & svg {
+        height: 1.9em;
+      }
+    }
+  }
 `;
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <SectionContent>
-      <LinkContainer>
+      {!open && <HamburgerMenu onClick={() => setOpen(true)} />}
+      <LinkContainer open={open}>
+        {open && <FaTimes onClick={() => setOpen(false)} size="1.5em" />}
         <Link target="_blank" href="https://github.com/timjacksonm">
           <GithubIcon />
         </Link>
@@ -47,6 +98,10 @@ const Navbar = () => {
         <Link target="_blank" href="https://www.linkedin.com/in/timjacksonm/">
           <FaLinkedin color="#0e76a8" />
         </Link>
+        <Link href="/">
+          <FaFilePdf color=" #272341" />
+        </Link>
+        <Line />
       </LinkContainer>
     </SectionContent>
   );
