d69e30f588acef5d95266227b455aede4cfd3653
Tim Jackson <timjacksonm@gmail.com>
2021-11-17 19:00:09 -0600

    removed react-ga instead just copied ga gtag into head of html

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-213300202-1');
 
 ReactDOM.render(
   <React.StrictMode>
